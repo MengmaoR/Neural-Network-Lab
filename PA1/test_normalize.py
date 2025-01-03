8a5ac966cbcafaa8ed7c88e5b51691ccb984a9f9
MengmaoR <tianxj0520@163.com>
2025-01-03 13:21:19 +0800

    nrom

--- PA1/test_normalize.py
+++ PA1/test_normalize.py
@@ -12,57 +12,57 @@ args['dataset'] = 'cifar100'
 args['batch_size'] = 64
 train.args = args
 train.main()
-none_64_loss, none_64_conv = train.loss_log, train.acc_log
+none_64_loss, none_64_acc = train.loss_log, train.acc_log
 train.loss_log, train.acc_log = [], []
 
 # bn 归一化，batch_size = 64
 args['normalization'] = 'bn'
 train.args = args
 train.main()
-bn_64_loss, bn_64_conv = train.loss_log, train.acc_log
+bn_64_loss, bn_64_acc = train.loss_log, train.acc_log
 train.loss_log, train.acc_log = [], []
 
 # ln 归一化，batch_size = 64
 args['normalization'] = 'ln'
 train.args = args
 train.main()
-ln_64_loss, ln_64_conv = train.loss_log, train.acc_log
+ln_64_loss, ln_64_acc = train.loss_log, train.acc_log
 train.loss_log, train.acc_log = [], []
 
 # gn 归一化，batch_size = 64
 args['normalization'] = 'gn'
 train.args = args
 train.main()
-gn_64_loss, gn_64_conv = train.loss_log, train.acc_log
+gn_64_loss, gn_64_acc = train.loss_log, train.acc_log
 train.loss_log, train.acc_log = [], []
 
-# 无归一化，batch_size = 128
-args['batch_size'] = 128
+# 无归一化，batch_size = 16
+args['batch_size'] = 16
 args['normalization'] = 'none'
 train.args = args
 train.main()
-none_128_loss, none_128_conv = train.loss_log, train.acc_log
+none_16_loss, none_16_acc = train.loss_log, train.acc_log
 train.loss_log, train.acc_log = [], []
 
-# bn 归一化，batch_size = 128
+# bn 归一化，batch_size = 16
 args['normalization'] = 'bn'
 train.args = args
 train.main()
-bn_128_loss, bn_128_conv = train.loss_log, train.acc_log
+bn_16_loss, bn_16_acc = train.loss_log, train.acc_log
 train.loss_log, train.acc_log = [], []
 
-# ln 归一化，batch_size = 128
+# ln 归一化，batch_size = 16
 args['normalization'] = 'ln'
 train.args = args
 train.main()
-ln_128_loss, ln_128_conv = train.loss_log, train.acc_log
+ln_16_loss, ln_16_acc = train.loss_log, train.acc_log
 train.loss_log, train.acc_log = [], []
 
-# gn 归一化，batch_size = 128
+# gn 归一化，batch_size = 16
 args['normalization'] = 'gn'
 train.args = args
 train.main()
-gn_128_loss, gn_128_conv = train.loss_log, train.acc_log
+gn_16_loss, gn_16_acc = train.loss_log, train.acc_log
 train.loss_log, train.acc_log = [], []
 
 # 无归一化，batch_size = 256
@@ -105,15 +105,15 @@ plt.legend()
 plt.savefig(f'./PA1/img/loss_norm_{args["dataset"]}_{args["model"]}_64.png', dpi = 600)
 plt.clf()
 
-plt.plot(none_128_loss, "o", color='blue', alpha=0.2, label='None 128')
-plt.plot(bn_128_loss, "o", color='red', alpha=0.2, label='BN 128')
-plt.plot(ln_128_loss, "o", color='orange', alpha=0.2, label='LN 128')
-plt.plot(gn_128_loss, "o", color='green', alpha=0.2, label='GN 128')
+plt.plot(none_16_loss, "o", color='blue', alpha=0.2, label='None 16')
+plt.plot(bn_16_loss, "o", color='red', alpha=0.2, label='BN 16')
+plt.plot(ln_16_loss, "o", color='orange', alpha=0.2, label='LN 16')
+plt.plot(gn_16_loss, "o", color='green', alpha=0.2, label='GN 16')
 plt.xlabel('Batches')
 plt.ylabel('Loss')
 plt.title('Training Loss over Batches')
 plt.legend()
-plt.savefig(f'./PA1/img/loss_norm_{args["dataset"]}_{args["model"]}_128.png', dpi = 600)
+plt.savefig(f'./PA1/img/loss_norm_{args["dataset"]}_{args["model"]}_16.png', dpi = 600)
 plt.clf()
 
 plt.plot(none_256_loss, "o", color='blue', alpha=0.2, label='None 256')
@@ -128,10 +128,10 @@ plt.savefig(f'./PA1/img/loss_norm_{args["dataset"]}_{args["model"]}_256.png', dp
 plt.clf()
 
 # Plot acc_log
-plt.plot(none_64_conv, label='None 64', color='blue')
-plt.plot(bn_64_conv, label='BN 64', color='red')
-plt.plot(ln_64_conv, label='LN 64', color='orange')
-plt.plot(gn_64_conv, label='GN 64', color='green')
+plt.plot(none_64_acc, label='None 64', color='blue')
+plt.plot(bn_64_acc, label='BN 64', color='red')
+plt.plot(ln_64_acc, label='LN 64', color='orange')
+plt.plot(gn_64_acc, label='GN 64', color='green')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.title('Test Accuracy over Epochs')
@@ -139,15 +139,15 @@ plt.legend()
 plt.savefig(f'./PA1/img/acc_norm_{args["dataset"]}_{args["model"]}_64.png', dpi = 600)
 plt.clf()
 
-plt.plot(none_128_conv, label='None 128', color='blue')
-plt.plot(bn_128_conv, label='BN 128', color='red')
-plt.plot(ln_128_conv, label='LN 128', color='orange')
-plt.plot(gn_128_conv, label='GN 128', color='green')
+plt.plot(none_16_acc, label='None 16', color='blue')
+plt.plot(bn_16_acc, label='BN 16', color='red')
+plt.plot(ln_16_acc, label='LN 16', color='orange')
+plt.plot(gn_16_acc, label='GN 16', color='green')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.title('Test Accuracy over Epochs')
 plt.legend()
-plt.savefig(f'./PA1/img/acc_norm_{args["dataset"]}_{args["model"]}_128.png', dpi = 600)
+plt.savefig(f'./PA1/img/acc_norm_{args["dataset"]}_{args["model"]}_16.png', dpi = 600)
 plt.clf()
 
 plt.plot(none_256_conv, label='None 256', color='blue')
